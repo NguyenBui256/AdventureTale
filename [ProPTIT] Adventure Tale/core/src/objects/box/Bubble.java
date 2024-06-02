8fbe54cd3670bb21f1e3ecef5fa9f6b1365fdd61
minhengdey <dlminhanh272@gmail.com>
2024-06-02 18:40:39 +0700

    feat: update door animation

--- [ProPTIT] Adventure Tale/core/src/objects/box/Bubble.java
+++ [ProPTIT] Adventure Tale/core/src/objects/box/Bubble.java
@@ -18,25 +18,15 @@ public class Bubble extends Sprite {
     public TextureRegion[][] region;
     public Texture texture;
 
-    public boolean isDoor = false;
-
     public Bubble(GameScreen screen, Body body, String imagePath, int width, int height){
         this.body = body;
         texture = new Texture(imagePath);
         region = TextureRegion.split(texture, width, height);
         Animation animation = new Animation(0.3f, region[0]);
         setRegion((TextureRegion) animation.getKeyFrame(screen.stateTime));
-        for(Fixture fixture : body.getFixtureList()){
-            if(fixture.getUserData() == "door"){
-                isDoor = true;
-                break;
-            }
-        }
-        if(!isDoor) setBounds(body.getPosition().x,body.getPosition().y,1.4f*tiledSize/PPM, 1.4f*tiledSize/PPM);
-        else setBounds(body.getPosition().x,body.getPosition().y,2*tiledSize/PPM, 2*tiledSize/PPM);
+        setBounds(body.getPosition().x,body.getPosition().y,1.4f*tiledSize/PPM, 1.4f*tiledSize/PPM);
     }
     public void update(float dt){
-        if(!isDoor) setPosition(body.getPosition().x - getWidth() / 2, body.getPosition().y - getHeight() / 5);
-        else setPosition(body.getPosition().x - getWidth() / 2, body.getPosition().y - getHeight() / 2);
+        setPosition(body.getPosition().x - getWidth() / 2, body.getPosition().y - getHeight() / 5);
     }
 }
